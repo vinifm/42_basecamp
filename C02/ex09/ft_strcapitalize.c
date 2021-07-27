bd5f87862881fab592c53a0b10b73e5d0857bbd2
viferrei <viferrei@student.42sp.org.br>
2021-07-27 14:39:31 +0000

    correct ex09 after mouli

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -5,22 +5,18 @@ char	*ft_strcapitalize(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (!(str[i - 1] >= '0' && str[i - 1] <= '9'))
+		if (str[i] >= 'A' && str[i] <= 'Z')
 		{
-			if (!(str[i - 1] >= 'A' && str[i - 1] <= 'Z'))
-			{
-				if (!(str[i - 1] >= 'a' && str[i - 1] <= 'z'))
-				{
-					if (str[i] >= 'a' && str[i] <= 'z')
-					{
-						str[i] = str[i] - 32;
-					}
-				}
-			}
+			str[i] = str[i] + 32;
+		}
+		if (!(str[i - 1] >= '0' && str[i - 1] <= '9')
+			&& !(str[i - 1] >= 'A' && str[i - 1] <= 'Z')
+			&& !(str[i - 1] >= 'a' && str[i - 1] <= 'z')
+			&& str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
 		}
 		i++;
 	}
 	return (str);
 }
-
-// must leave rest of world in lowcase :(
